cb2dd8cd448848aa7f9e6e15a25cd4d3d5217dae
SniperX <40890777+MaelstromDota@users.noreply.github.com>
2021-05-26 20:42:54 +0600

    alt+click buff

--- panorama/scripts/custom_game/buffs.js
+++ panorama/scripts/custom_game/buffs.js
@@ -9,22 +9,28 @@ function Main(){
 	};
 	for (let i=0; i < buffs.length; i++){
 		var pPanel = pContainer.GetChild(i);
+		let stacks = Buffs.GetStackCount(unit, buffs[i]);
+		let stack = stacks < 1 ? '' : stacks;
 		if (pPanel == null || pPanel == undefined) {
 			pPanel = $.CreatePanel("Panel", pContainer, "");
 			pPanel.BLoadLayoutSnippet("Buff");
 			let dpanel = pPanel.FindChildTraverse("buffid");
 			dpanel.SetPanelEvent("onmouseover", function(){$.DispatchEvent("DOTAShowBuffTooltip", dpanel, unit, buffs[i], false);})
 			dpanel.SetPanelEvent("onmouseout", function(){$.DispatchEvent("DOTAHideBuffTooltip", dpanel);})
+			dpanel.SetPanelEvent("onactivate", function(){
+				if (GameUI.IsAltDown()){
+					Game.ServerCmd(`say_team ${$.Localize('#i_under_buff')} ${stack} ${$.Localize('#DOTA_Tooltip_'+Buffs.GetName(unit, buffs[i]))}`);
+				};
+			})
 		};
-		pPanel.style.marginLeft = `${42 * i}px`;
+		pPanel.style.marginLeft = `${48 * i}px`;
 		let border = pPanel.FindChildTraverse("border");
 		let elapsed = Buffs.GetElapsedTime(unit, buffs[i]);
 		border.style.clip = `radial(50% 50%, 0deg, ${360 - elapsed * 18}deg)`;
 		let stackstext = pPanel.FindChildTraverse("stacks");
-		let stacks = Buffs.GetStackCount(unit, buffs[i]);
 		if (stacks < 1) {stackstext.style.visibility = 'collapse';} else {stackstext.style.visibility = 'visible'; stackstext.text = stacks;};
-		if (Buffs.IsDebuff(unit,buffs[i])) {border.SetImage("file://{images}/hud/border_debuff.png")}
-		pPanel.FindChildTraverse("image").SetImage(`s2r://panorama/images/spellicons/${Buffs.GetTexture(unit,buffs[i])}_png.vtex`)
+		if (Buffs.IsDebuff(unit,buffs[i])) {border.SetImage("file://{images}/hud/border_debuff.png");};
+		pPanel.FindChildTraverse("image").SetImage(`s2r://panorama/images/spellicons/${Buffs.GetTexture(unit,buffs[i])}_png.vtex`);
 	};
 	for (let i = buffs.length; i < pContainer.GetChildCount(); i++) {
 		let pPanel = pContainer.GetChild(i);
