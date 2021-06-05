0ef966f79fa640102b3094e3c16ed659f72cf5a0
SniperX <40890777+MaelstromDota@users.noreply.github.com>
2021-06-05 22:11:35 +0600

    fix

--- panorama/scripts/custom_game/actionpanel/lvlup.js
+++ panorama/scripts/custom_game/actionpanel/lvlup.js
@@ -1,95 +1,85 @@
 var LearnActive = false;
 var SaveAbilityList = [];
 (function () {
-    SearchForSkills();
+	SearchForSkills();
 })();
 function SearchForSkills() {
-    SaveAbilityList = [];
-    for (let i = 0; i <= 32; i++) {
-        var Aentity = Entities.GetAbility(Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()), i);
-        var Aname = Abilities.GetAbilityName(Aentity);
-        if (Abilities.IsDisplayedAbility(Aentity) && Aname != 'attribute_bonus_datadriven') {SaveAbilityList.push(i);};
-    };
+	SaveAbilityList = [];
+	for (let i = 0; i <= 32; i++) {
+		var Aentity = Entities.GetAbility(Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()), i);
+		var Aname = Abilities.GetAbilityName(Aentity);
+		if (Abilities.IsDisplayedAbility(Aentity) && Aname != 'attribute_bonus_datadriven') {SaveAbilityList.push(i);};
+	};
 };
 function OnLVLButtonPressed() {
-    LearnActive = !LearnActive;
-    PointCheker();
+	LearnActive = !LearnActive;
+	PointCheker();
 };
 function PointCheker() {
-    if (Entities.GetAbilityPoints(Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer())) != 0 && Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()) == Players.GetLocalPlayerPortraitUnit()) {
-        $('#LevelUpButton').style.opacity = 1;
-        if (LearnActive) {
-            let AbilityUp = [];
-            for (let i = 0; i < SaveAbilityList.length; i++) {
-                var Aentity = Entities.GetAbility(Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()), SaveAbilityList[i]);
-                if (Abilities.CanAbilityBeUpgraded(Aentity) == 0 && Entities.GetLevel(Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer())) >= Abilities.GetHeroLevelRequiredToUpgrade(Aentity)) {
-                    if (Abilities.GetAbilityType(Aentity) == 1) {
-                        AbilityUp.push(AbilityUp[AbilityUp.length - 1] + 1);
-                    } else {
-                        AbilityUp.push(i);
-                    };
-                };
-            };
-            HideUpgradableAbilities();
-            VisibleLvlUpBacklighting(AbilityUp);
-        } else {
-            HideUpgradableAbilities();
-        };
-    } else {
-        LearnActive = false;
-        $('#LevelUpButton').style.opacity = 0;
-        HideUpgradableAbilities();
-    };
-    let AllAbilities = $('#AbilitiesContainer').Children();
-    for (let i = 0; i < AllAbilities.length; i++) {
-        let ability = Entities.GetAbility(Players.GetLocalPlayerPortraitUnit(), i);
-        if (ability != undefined && Abilities.GetLevel(ability) > 0) {
-            AllAbilities[i].FindChildTraverse("AbilityImage").style.saturation = '1';
-            AllAbilities[i].FindChildTraverse("AbilityImage").style.brightness = '1';
-        };
-    };
-    $.Schedule(0.5, PointCheker);
+	if (Entities.GetAbilityPoints(Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer())) != 0 && Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()) == Players.GetLocalPlayerPortraitUnit()) {
+		$('#LevelUpButton').style.opacity = 1;
+		if (LearnActive) {
+			let AbilityUp = [];
+			for (let i = 0; i < SaveAbilityList.length; i++) {
+				var Aentity = Entities.GetAbility(Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()), SaveAbilityList[i]);
+				if (Abilities.CanAbilityBeUpgraded(Aentity) == 0 && Entities.GetLevel(Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer())) >= Abilities.GetHeroLevelRequiredToUpgrade(Aentity)) {
+					if (Abilities.GetAbilityType(Aentity) == 1) {
+						AbilityUp.push(AbilityUp[AbilityUp.length - 1] + 1);
+					} else {
+						AbilityUp.push(i);
+					};
+				};
+			};
+			HideUpgradableAbilities();
+			VisibleLvlUpBacklighting(AbilityUp);
+		} else {
+			HideUpgradableAbilities();
+		};
+	} else {
+		LearnActive = false;
+		$('#LevelUpButton').style.opacity = 0;
+		HideUpgradableAbilities();
+	};
 };
 function VisibleLvlUpBacklighting(AbilityUp) {
-    let AllAbilities = $('#AbilitiesContainer').Children();
-    for (let i = 0; i < AbilityUp.length; i++) {
-        AllAbilities[AbilityUp[i]].FindChildTraverse("AbilityImage").style.saturation = '1';
-        AllAbilities[AbilityUp[i]].FindChildTraverse("AbilityImage").style.brightness = '1';
-        AllAbilities[AbilityUp[i]].FindChildTraverse("LearnOverlay").style.visibility = 'visible';
-    };
+	let AllAbilities = $('#AbilitiesContainer').Children();
+	for (let i = 0; i < AbilityUp.length; i++) {
+		AllAbilities[AbilityUp[i]].FindChildTraverse("AbilityImage").RemoveClass("NotLearned");
+		AllAbilities[AbilityUp[i]].FindChildTraverse("LearnOverlay").style.visibility = 'visible';
+	};
 };
 function HideUpgradableAbilities() {
-    let AllAbilities = $('#AbilitiesContainer').Children();
-    for (let i = 0; i < AllAbilities.length; i++) {
-        AllAbilities[i].FindChildTraverse("LearnOverlay").style.visibility = 'collapse';
-        if (Abilities.GetLevel(Entities.GetAbility( Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()), i )) < 1) {
-            AllAbilities[i].FindChildTraverse("AbilityImage").style.saturation = '0';
-            AllAbilities[i].FindChildTraverse("AbilityImage").style.brightness = '0.03';
-        };
-    };
+	let AllAbilities = $('#AbilitiesContainer').Children();
+	if (Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()) == Players.GetLocalPlayerPortraitUnit()) {
+		for (let i = 0; i < AllAbilities.length; i++) {
+			AllAbilities[i].FindChildTraverse("LearnOverlay").style.visibility = 'collapse';
+			if (Abilities.GetLevel(Entities.GetAbility(Players.GetLocalPlayerPortraitUnit(), i)) < 1) {
+				AllAbilities[i].FindChildTraverse("AbilityImage").AddClass("NotLearned");
+			};
+		};
+	};
 };
 function AddLearnSkill() {
-    SearchForSkills();
-    let AllAbilities = $('#AbilitiesContainer').Children();
-    for (let i = 0; i < SaveAbilityList.length; i++) {
-        AllAbilities[i].FindChildTraverse("LearnOverlay").SetPanelEvent('onactivate', function () {
-            Game.PrepareUnitOrders({OrderType: dotaunitorder_t.DOTA_UNIT_ORDER_TRAIN_ABILITY, UnitIndex: Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()), AbilityIndex: Entities.GetAbility( Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()), SaveAbilityList[i] ), Queue: OrderQueueBehavior_t.DOTA_ORDER_QUEUE_NEVER, ShowEffects: false});
-        });
-    };
+	SearchForSkills();
+	let AllAbilities = $('#AbilitiesContainer').Children();
+	for (let i = 0; i < SaveAbilityList.length; i++) {
+		AllAbilities[i].FindChildTraverse("LearnOverlay").SetPanelEvent('onactivate', function () {
+			Game.PrepareUnitOrders({OrderType: dotaunitorder_t.DOTA_UNIT_ORDER_TRAIN_ABILITY, UnitIndex: Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()), AbilityIndex: Entities.GetAbility( Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()), SaveAbilityList[i] ), Queue: OrderQueueBehavior_t.DOTA_ORDER_QUEUE_NEVER, ShowEffects: false});
+		});
+	};
 };
 function UpdateSelectedUnit() {
-    if (Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()) != Players.GetLocalPlayerPortraitUnit()) {
-        LearnActive = false;
-        HideUpgradableAbilities();
-    };
+	if (Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()) != Players.GetLocalPlayerPortraitUnit()) {
+		LearnActive = false;
+	};
 };
 GameEvents.Subscribe("dota_player_learned_ability", PointCheker );
 GameEvents.Subscribe("dota_player_gained_level", PointCheker );
 GameEvents.Subscribe("dota_player_update_selected_unit", UpdateSelectedUnit );
 function OnPanelLoaded() {
-    $.Schedule(3, function () {
-        SearchForSkills();
-        PointCheker();
-        AddLearnSkill();
-    }); 
+	$.Schedule(3, function () {
+		SearchForSkills();
+		PointCheker();
+		AddLearnSkill();
+	}); 
 };
