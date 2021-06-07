6fee89e9681702e5d4d8fa5a01a95c55bb128880
SniperX <40890777+MaelstromDota@users.noreply.github.com>
2021-06-07 12:08:58 +0600

    fixed lvlup button localization

--- panorama/scripts/custom_game/actionpanel/lvlup.js
+++ panorama/scripts/custom_game/actionpanel/lvlup.js
@@ -3,6 +3,7 @@ function PointCheker() {
 	if (Entities.GetAbilityPoints(Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer())) > 0 && Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()) == Players.GetLocalPlayerPortraitUnit()) {
 		$('#LevelUpButton').style.opacity = 1;
 		$('#Hotkey').style.visibility = 'visible';
+		$('#LVLUPText').text = `${$.Localize("#DOTA_LevelUp")} +${Entities.GetAbilityPoints(Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer()))}`;
 		$('#Hotkey').text = Game.GetKeybindForCommand(DOTAKeybindCommand_t.DOTA_KEYBIND_LEARN_ABILITIES).toString();
 	} else {
 		$('#Hotkey').style.visibility = 'collapse';
