35f601bbacd8a04a9fea7be4838dd64099cfd281
SniperX <40890777+MaelstromDota@users.noreply.github.com>
2021-06-07 13:18:23 +0600

    small changes with quickbuy rows

--- panorama/scripts/custom_game/removetrash.js
+++ panorama/scripts/custom_game/removetrash.js
@@ -16,6 +16,10 @@ function removetrash(){
 	newUI.FindChildTraverse('QuickBuyRows').style.horizontalAlign = 'right';
 	newUI.FindChildTraverse('QuickBuyRows').style.backgroundColor = "#000000";
 	newUI.FindChildTraverse('QuickBuyRows').FindChildTraverse("Hint").style.marginLeft = "10px";
+	newUI.FindChildTraverse('QuickBuyRows').FindChildTraverse("ClearQuickBuy").style.marginRight = "12px";
+	newUI.FindChildTraverse('QuickBuyRows').FindChildTraverse("ClearQuickBuy").style.marginTop = "45px";
+	newUI.FindChildTraverse('QuickBuyRows').FindChildTraverse("QuickBuySlot8").style.verticalAlign = "top";
+	newUI.FindChildTraverse('QuickBuyRows').FindChildTraverse("QuickBuySlot8").style.marginTop = "10px";
 	newUI.FindChildTraverse('QuickBuyRows').FindChildTraverse("StickyItemSlotContainer").style.backgroundColor = "#00000000";
 	newUI.FindChildTraverse('lower_hud').FindChildTraverse('center_block').FindChildTraverse('ButtonAndLevel').style.visibility = 'collapse';
 	GameUI.SetDefaultUIEnabled( DotaDefaultUIElement_t.DOTA_DEFAULT_UI_ACTION_PANEL, false );
